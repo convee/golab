f291efeb897f2c1a795a394e0c945cd68a5eba35
convee <407532920@qq.com>
2018-05-16 23:34:28 +0800

    测试例子

---
+++ test/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+type student struct {
+	Name string
+	Age  int
+}
+
+func parseStudent() {
+	m := make(map[string]*student)
+	stus := []student{
+		student{Name: "wang1", Age: 13},
+		student{Name: "wang2", Age: 14},
+		student{Name: "wang3", Age: 15},
+	}
+	for k, stu := range stus {
+		m[stu.Name] = &stus[k]
+	}
+	fmt.Println(m)
+}
+
+func syncc() {
+	runtime.GOMAXPROCS(1)
+	wg := sync.WaitGroup{}
+	wg.Add(20)
+	for i := 0; i < 10; i++ {
+		go func() {
+			fmt.Println("i: ", i)
+			wg.Done()
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			fmt.Println("i: ", i)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+}
+
+func chanc() {
+	fmt.Println(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+func main() {
+	// parseStudent()
+	syncc()
+}
