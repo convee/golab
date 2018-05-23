295b53a96f4e1188caf53107f34639d9f2509b86
convee <407532920@qq.com>
2018-05-23 23:54:32 +0800

    选择排序

--- sort/ssort/main.go
+++ sort/ssort/main.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//选择排序
 func ssort(a []int) {
 
 	for i := 0; i < len(a); i++ {
@@ -15,8 +16,21 @@ func ssort(a []int) {
 	}
 }
 
+func ssort1(a []int) {
+	lenA := len(a)
+	for i := 0; i < lenA; i++ {
+		var min int = i
+		for j := i + 1; j < lenA; j++ {
+			if a[j] < a[min] {
+				min = j
+			}
+		}
+		a[i], a[min] = a[min], a[i]
+
+	}
+}
 func main() {
 	b := [...]int{8, 7, 5, 4, 3, 10, 15}
-	ssort(b[:])
+	ssort1(b[:])
 	fmt.Println(b)
 }
