133874be1a9a64f7f3b78a116b3e513654dcd74f
convee <407532920@qq.com>
2018-05-23 23:17:57 +0800

    sort

--- sort/bsort/main.go
+++ sort/bsort/main.go
@@ -12,9 +12,18 @@ func bsort(a []int) {
 		}
 	}
 }
-
+func bsort1(a []int) {
+	lenA := len(a)
+	for i := 0; i < lenA; i++ {
+		for j := 0; j < lenA-i-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+			}
+		}
+	}
+}
 func main() {
 	b := [...]int{8, 7, 5, 4, 3, 10, 15}
-	bsort(b[:])
+	bsort1(b[:])
 	fmt.Println(b)
 }
