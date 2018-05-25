067a1de32b281efb10831ced2710d183249b9551
wangkang <wangkang@douyu.tv>
2018-05-25 15:40:51 +0800

    context

---
+++ context/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"sync"
+)
+
+func  HTTPAPIServe(ctx context.Context)  {
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	ctx,cancel  := context.WithCancel(ctx)
+	wg := &sync.WaitGroup{}
+	defer wg.Wait()
+	wg.Add(1)
+	var server *http.Server
+	go func(ctx context.Context) {
+		defer wg.Done()
+		defer cancel()
+		server = &http.Server{Addr:"127.0.0.1:8000", Handler:nil}
+		_ = server.ListenAndServe()
+	}(ctx)
+	select {
+	case <-ctx.Done():
+		server.Close()
+
+	}
+}
+func main(){
+	wg:=sync.WaitGroup{}
+	defer wg.Wait()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		defer cancel()
+		HTTPAPIServe(ctx)
+	}(ctx)
+
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		defer cancel()
+		//todo
+	}(ctx)
+
+}
