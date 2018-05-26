14a21fbe79b50c903c1c05d462eab6da450e5618
convee <407532920@qq.com>
2018-05-26 22:48:16 +0800

    context

--- md5/main.go
+++ md5/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func MD5(text string) string {
+	ctx := md5.New()
+	ctx.Write([]byte(text))
+	return hex.EncodeToString(ctx.Sum(nil))
+
+}
+
+func GetRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+
+}
 func main() {
 	pwd := "123456"
 	md5 := fmt.Sprintf("%x", md5.Sum([]byte(pwd)))
