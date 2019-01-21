0232b9a21982db2d08927f00ee17b96645a7f3cc
convee <convee.cn@gmail.com>
2019-01-22 04:06:22 +0800

    map排序

---
+++ leetcode/remove-duplicates/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+//双指针法
+//数组完成排序后，我们可以放置两个指针 i 和 j，其中 i 是慢指针，而 j 是快指针。只要 nums[i] = nums[j]，我们就增加 j 以跳过重复项。
+
+//当我们遇到 nums[j] != nums[i] 时，跳过重复项的运行已经结束，因此我们必须把它（nums[j]）的值复制到 nums[i + 1]。然后递增 i，接着我们将再次重复相同的过程，直到 j 到达数组的末尾为止。
+func removeDuplicates(nums []int) int {
+	var i int
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != nums[i] {
+			i++
+			nums[i] = nums[j]
+		}
+	}
+	return i + 1
+
+}
+
+func main() {
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	fmt.Println(removeDuplicates(nums))
+}
--- map/main.go
+++ map/main.go
@@ -1,11 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
+
+//map按照key顺序读取
+func sortMapByKey(maps map[string]string) {
+	keys := []string{}
+	for k := range maps {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Println("key:", k, "value:", maps[k])
+	}
+}
+
+func sortMapByValue(maps map[string]string) {
+	values := []string{}
+	newMaps := make(map[string]string)
+	for k, v := range maps {
+		values = append(values, v)
+		newMaps[v] = k
+	}
+	sort.Strings(values)
+	for _, v := range values {
+		fmt.Println("key:", newMaps[v], "value:", v)
+	}
+}
 
 //map的key是无序的
 func main() {
-	dist := make(map[int]string)
-	dist[1] = "666"
-	dist[2] = "777"
+	dist := make(map[string]string)
+	dist["1"] = "666"
+	dist["2"] = "777"
+	dist["4"] = "555"
 	fmt.Println(dist)
+	sortMapByKey(dist)
+	sortMapByValue(dist)
+
 }
