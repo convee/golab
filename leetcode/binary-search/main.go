1b6d8fa39de2468d6abab93fbe4777c5928b3eac
convee <convee.cn@gmail.com>
2019-02-23 22:40:22 +0800

    二叉树非递归

--- leetcode/binary-search/main.go
+++ leetcode/binary-search/main.go
@@ -2,12 +2,9 @@ package main
 
 import "fmt"
 
-func search(nums []int, target int) int {
-	return binSearch(nums, 0, len(nums)-1, target)
-}
-
+//第归方式完成二分查找
 func binSearch(nums []int, start int, end int, target int) int {
-	mid := (int)((start + end) / 2)
+	mid := int((start + end) / 2)
 	if start <= end {
 		if nums[mid] == target {
 			return mid
@@ -19,8 +16,23 @@ func binSearch(nums []int, start int, end int, target int) int {
 	}
 	return -1
 }
+
+//非递归方式完成二分查找
+func binSearch2(nums []int, start int, end int, target int) int {
+	for start <= end {
+		mid := int((start + end) / 2)
+		if target == nums[mid] {
+			return mid
+		} else if target > nums[mid] {
+			start = mid + 1
+		} else {
+			end = mid - 1
+		}
+	}
+	return -1
+}
 func main() {
 	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	target := 9
-	fmt.Println(search(nums, target))
+	fmt.Println(binSearch2(nums, 0, len(nums)-1, target))
 }
