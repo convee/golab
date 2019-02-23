e10e58470e3b7e7ac5793f49788217ced7937546
convee <convee.cn@gmail.com>
2019-02-23 18:43:08 +0800

    golang字符串反转

--- string/reverse/main.go
+++ string/reverse/main.go
@@ -14,6 +14,15 @@ func reverseString(s string) string {
 	return string(r)
 }
 
+func reverseString2(s string) string {
+	r := []rune(s)
+	l := len(r)
+	new := []rune{}
+	for i := l - 1; i >= 0; i-- {
+		new = append(new, r[i])
+	}
+	return string(new)
+}
 func main() {
 	s := "hello golang语言"
 	fmt.Println(reverseString(s))
